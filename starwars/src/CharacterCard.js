89a0036576ea5a5c313cd58f4796e390ed86f4bb
Kevin Allen <kevin.allen91@gmail.com>
2019-09-13 11:39:13 -0700

    css to styled-components

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from "styled-components"
+
+const cardStyle = styled.div `
+  width:25vw;
+  border:black 1px solid;
+  color:white;
+  text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+  margin: 5vh;
+
+`;
 
 const CharacterCard = (props) => {
   return (
+
     
       
-        <div className="cards" key={props.index}>
+        <cardStyle className="cards" key={props.index}>
           <h2 className="name">{props.name}</h2>
           <p className="birth">Born: {props.birth_year}</p>
           <p className="hair">Hair Color: {props.hair_color}</p>
-        </div>
+        </cardStyle>
       
     
   );
