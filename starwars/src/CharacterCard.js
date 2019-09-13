c01e824f9d7e3031b209a713c33eeb2fe593a06e
Kevin Allen <kevin.allen91@gmail.com>
2019-09-13 11:40:38 -0700

    fixed react variable error

--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components"
 
-const cardStyle = styled.div `
+const CardStyle = styled.div `
   width:25vw;
   border:black 1px solid;
   color:white;
@@ -15,11 +15,11 @@ const CharacterCard = (props) => {
 
     
       
-        <cardStyle className="cards" key={props.index}>
+        <CardStyle className="cards" key={props.index}>
           <h2 className="name">{props.name}</h2>
           <p className="birth">Born: {props.birth_year}</p>
           <p className="hair">Hair Color: {props.hair_color}</p>
-        </cardStyle>
+        </CardStyle>
       
     
   );
